3239b05d98e794fe1f49345ad9aaa8f10def8bf8
komihori <komiho.ri@gmail.com>
2022-03-15 22:57:21 +0900

    Update ADX2Manager.cs

--- ADX2Manager.cs
+++ ADX2Manager.cs
@@ -7,18 +7,16 @@ public class ADX2Manager : MonoBehaviour
     public static ADX2Manager Instance { get; private set; }
     private CriAtomExPlayer exPlayer, exPlayer2, exSePlayer;
     private CriAtomExAcb bgmAcb, seAcb;
+    public CriAtomExPlayback playBack { get; private set; }
     private int fadeInTime = 1000;
     private int fadeOutTime = 1000;
+    public static bool CueSheetLoaded { get; private set; } = false;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
+    private void Awake() {
+        if (ADX2LEManager.Instance == null) {
+            ADX2LEManager.Instance = this;
             DontDestroyOnLoad(this);
-        }
-        else
-        {
+        } else {
             Destroy(this);
             return;
         }
@@ -38,44 +36,50 @@ public class ADX2Manager : MonoBehaviour
         exSePlayer = new CriAtomExPlayer();
     }
 
-    private IEnumerator Start()
-    {
+    private IEnumerator Start() {
         yield return new WaitUntil(() => !CriAtom.CueSheetsAreLoading);
 
         bgmAcb = CriAtom.GetAcb("BGM");
         seAcb = CriAtom.GetAcb("SE");
+        
+        CueSheetLoaded = true;
     }
 
-    public void PlayBGM(string _CueName)
-    {
-        if (exPlayer.GetStatus() != CriAtomExPlayer.Status.Playing)
-        {
-            if (exPlayer2.GetStatus() == CriAtomExPlayer.Status.Playing)
+    public void PlayBGM(string _CueName) {
+        if (exPlayer.GetStatus() != CriAtomExPlayer.Status.Playing) {
+            if (exPlayer2.GetStatus() == CriAtomExPlayer.Status.Playing) {
                 exPlayer2.Stop();
+            }
+
             exPlayer.SetCue(bgmAcb, _CueName);
-            exPlayer.Start();
+            playBack = exPlayer.Start();
             return;
-        }
-        else if (exPlayer2.GetStatus() != CriAtomExPlayer.Status.Playing)
-        {
-            if (exPlayer.GetStatus() == CriAtomExPlayer.Status.Playing)
+        } else if (exPlayer2.GetStatus() != CriAtomExPlayer.Status.Playing) {
+            if (exPlayer.GetStatus() == CriAtomExPlayer.Status.Playing) {
                 exPlayer.Stop();
+            }
+
             exPlayer2.SetCue(bgmAcb, _CueName);
-            exPlayer2.Start();
+            playBack = exPlayer2.Start();
             return;
+        } else {
+            if (exPlayer2.GetStatus() == CriAtomExPlayer.Status.Playing) {
+                exPlayer2.Stop();
+            }
+
+            exPlayer.SetCue(bgmAcb, _CueName);
+            playBack = exPlayer.Start();
         }
     }
 
-    public void StopAllBGM()
-    {
+    public void StopAllBGM() {
         if (exPlayer.GetStatus() == CriAtomExPlayer.Status.Playing)
             exPlayer.Stop();
         if (exPlayer2.GetStatus() == CriAtomExPlayer.Status.Playing)
             exPlayer2.Stop();
     }
 
-    public void SetAisacValue(string name, float value)
-    {
+    public void SetAisacValue(string name, float value) {
         if (exPlayer.GetStatus() == CriAtomExPlayer.Status.Playing)
         {
             exPlayer.SetAisacControl(name, value);
@@ -88,10 +92,23 @@ public class ADX2Manager : MonoBehaviour
         }
     }
 
-    public void PlaySE(string name)
-    {
+    public void PlaySE(string name) {
         exSePlayer.SetCue(seAcb, name);
         exSePlayer.Start();
     }
-
+    
+    public bool PauseNowPlayBGM() {
+        playBack.Pause();
+        return playBack.IsPaused();
+    }
+    
+    public bool ResumeNowPauseBGM() {
+        playBack.Resume(CriAtomEx.ResumeMode.PausedPlayback);
+        return playBack.IsPaused();
+    }
+    
+    public void SetCategoryVolume(string name, float volume) => CriAtom.SetCategoryVolume(name, volume);
+    
+    public void SetNextBlockIndex(int index) => playBack.SetNextBlockIndex(index);
+    public long GetPlayTime() => playBack.GetTime();
 }
